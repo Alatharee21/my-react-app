f6c0cac55b7f6ff30788398d30c24fd4986b1105
Rasheed <hidrahtweet@gmail.com>
2026-01-08 15:23:04 +0100

    added style attributes

--- src/Attributes.jsx
+++ src/Attributes.jsx
@@ -3,9 +3,17 @@ function Cclass(){
     const myFunc = () => {
         alert("Hello mfkr");
     }
+
+    //Styles are written as objects and attribute in camel case e.g fontSize instead of font-size
+    const myStyles = {
+        color: "red",
+        fontSize: "20px",
+        backgroundColor: "lightyellow"
+    };
+
     return(
         <>
-        <h1  className="melchi">Class name is string</h1>
+        <h1  className="melchi" style={myStyles}>Class name is string</h1>
         <h2 className={x}>class name is in const expression</h2>
         {/*disabled ={false} will able the button while disabled= {true} will disable it*/}
         <div><button onClick={myFunc} disabled>Click me  mfkr</button></div>
