ba062fc3007b67fb6c4f6743851fc61d4f197d5e
Rasheed <hidrahtweet@gmail.com>
2026-01-08 15:28:55 +0100

    added inline styling

--- src/Attributes.jsx
+++ src/Attributes.jsx
@@ -14,7 +14,7 @@ function Cclass(){
     return(
         <>
         <h1  className="melchi" style={myStyles}>Class name is string</h1>
-        <h2 className={x}>class name is in const expression</h2>
+        <h2 className={x} style={{color: "blue"}}>class name is in const expression</h2>{/*Styled thisinLine */}
         {/*disabled ={false} will able the button while disabled= {true} will disable it*/}
         <div><button onClick={myFunc} disabled>Click me  mfkr</button></div>
         <button onClick={myFunc}>Not disabled</button>
